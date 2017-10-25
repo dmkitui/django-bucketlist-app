58b51372354afa07806b7b8309ea0b1b196b3070
dmkitui <daniel.kitui@andela.com>
2017-10-25 17:50:56 +0300

    Add a create bucketlist view

--- bucketlistApi/views.py
+++ bucketlistApi/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .serializers import BucketlistsSerializer, UserSerializer
+from .models import Bucketlists
 
-# Create your views here.
+
+class CreateView(generics.ListCreateAPIView):
+    """Class for creating the bucketlists in the api."""
+    queryset = Bucketlists.objects.all()
+    serializer_class = BucketlistsSerializer
+
+    def perform_create(self, serializer):
+        """Save the post data when creating a new bucketlist."""
+        serializer.save()
