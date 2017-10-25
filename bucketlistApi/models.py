2f788ecd73b1831e33776ed156636ac9524f317b
dmkitui <daniel.kitui@andela.com>
2017-10-25 17:04:52 +0300

    Add skeleton User model

--- bucketlistApi/models.py
+++ bucketlistApi/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Bucketlists(models.Model):
+    """The bucketlist model."""
+    name = models.CharField(max_length=255, blank=False, unique=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        """Return a human readable representation of the model instance."""
+        return "{}".format(self.name)
+
+class User(models.Model):
+    """The User model"""
+    name = models.CharField(max_length=255, blank=False, unique=True)
+    date_registered = models.DateTimeField(auto_now_add=True)
