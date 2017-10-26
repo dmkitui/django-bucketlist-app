66bc7325a327f570db43db5f8e22142c14e1a99c
dmkitui <daniel.kitui@andela.com>
2017-10-26 21:53:53 +0300

    Add a method to generate a user token on user login

--- bucketlistApi/models.py
+++ bucketlistApi/models.py
@@ -1,5 +1,8 @@
 from django.db import models
-
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
+from django.dispatch import receiver
 
 class Bucketlists(models.Model):
     """The bucketlist model."""
@@ -11,3 +14,10 @@ class Bucketlists(models.Model):
     def __str__(self):
         """Return a human readable representation of the model instance."""
         return "{}".format(self.name)
+
+
+# Method to run on creation of a user
+@receiver(post_save, sender=User)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
