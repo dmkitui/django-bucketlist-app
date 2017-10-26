68b07e590869b4664aed8256e2a588444edb6ce4
dmkitui <daniel.kitui@andela.com>
2017-10-26 17:59:07 +0300

    Add Refactor the user model

--- bucketlistApi/models.py
+++ bucketlistApi/models.py
@@ -6,7 +6,7 @@ class Bucketlists(models.Model):
     name = models.CharField(max_length=255, blank=False, unique=True)
     date_created = models.DateTimeField(auto_now_add=True)
     date_modified = models.DateTimeField(auto_now=True)
-    owner = models.ForeignKey('auth.User', related_name='bucketlists', on_delete=models.CASCADE)
+    owner = models.ForeignKey('auth.User', related_name='bucketlists', null=True, blank=True, on_delete=models.CASCADE)
 
     def __str__(self):
         """Return a human readable representation of the model instance."""
