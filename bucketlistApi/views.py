3ffdab7070ae952eb38840d49a1dd2d3f6032faa
dmkitui <daniel.kitui@andela.com>
2017-10-26 21:07:32 +0300

    Include authentication and permission classes

--- bucketlistApi/views.py
+++ bucketlistApi/views.py
@@ -2,16 +2,19 @@ from django.shortcuts import render
 from rest_framework import generics
 from .serializers import BucketlistsSerializer
 from .models import Bucketlists
+from rest_framework import permissions
+from .permissions import IsOwner
 
 
 class CreateView(generics.ListCreateAPIView):
     """Class for creating the bucketlists in the api."""
     queryset = Bucketlists.objects.all()
     serializer_class = BucketlistsSerializer
+    permission_classes = (permissions.IsAuthenticated, IsOwner)
 
     def perform_create(self, serializer):
         """Save the post data when creating a new bucketlist."""
-        serializer.save()
+        serializer.save(owner=self.request.user)
 
 
 class DetailsView(generics.RetrieveUpdateDestroyAPIView):
