f35d23f309ed7fa67ce13f7461c610365c772bce
dmkitui <daniel.kitui@andela.com>
2017-10-26 17:19:53 +0300

    Refactor to remove User model

--- bucketlistApi/serializers.py
+++ bucketlistApi/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Bucketlists, User
+from .models import Bucketlists
 
 
 class BucketlistsSerializer(serializers.ModelSerializer):
@@ -10,13 +10,3 @@ class BucketlistsSerializer(serializers.ModelSerializer):
         model = Bucketlists
         fields = ('id', 'name', 'date_created', 'date_modified')
         read_only_fields = ('date_created', 'date_modified')
-
-
-class UserSerializer(serializers.ModelSerializer):
-    """Serializer to map the Model instance into JSON format."""
-
-    class Meta:
-        """Meta class to map serializer's fields with the User model fields."""
-        model = User
-        fields = ('id', 'user_name', 'date_registered')
-        read_only_fields = ('date_registered')
--- bucketlistApi/views.py
+++ bucketlistApi/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from rest_framework import generics
-from .serializers import BucketlistsSerializer, UserSerializer
+from .serializers import BucketlistsSerializer
 from .models import Bucketlists
 
 
